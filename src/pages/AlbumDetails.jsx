324e3ba020142068580874ac156def54cfae54fa
srushtikulkarni <srushtikulkarni09@gmail.com>
2026-01-29 16:11:38 +0530

    changes made

--- src/pages/AlbumDetails.jsx
+++ src/pages/AlbumDetails.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import '../css/AlbumDetails.css'
+
 
 const AlbumDetails = () => {
 
