8ec168ad8deccb728aa3e0da3aeeb8bbc9b1a717
srushtikulkarni <srushtikulkarni09@gmail.com>
2026-02-10 16:06:38 +0530

    changes made

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -22,25 +22,25 @@ const Login = () => {
     <div className="min-h-screen w-full flex justify-center items-center font-poppins relative overflow-hidden">
       <Navbar/>
 
-      <div className="absolute inset-0 bg-gradient-to-br from-indigo-600 via-purple-600 to-pink-500"></div>
+      <div className="absolute inset-0 bg-white"></div>
 
       <div className="absolute w-72 h-72 bg-white/20 rounded-full blur-3xl top-10 left-10 animate-pulse"></div>
       <div className="absolute w-80 h-80 bg-black/20 rounded-full blur-3xl bottom-10 right-10 animate-pulse"></div>
 
      {/* login  */}
-      <div className="relative z-10 bg-white/20 backdrop-blur-xl border border-white/30 rounded-2xl shadow-xl p-10 w-[350px] text-center">
+      <div className="relative z-10 bg-blue/20 backdrop-blur-xl border border-black/30 rounded-2xl shadow-xl p-10 w-[350px] text-center">
 
-        <h1 className="text-4xl font-pacifico text-white mb-2">
-          Snapify
+        <h1 className="text-4xl font-pacifico text-indigo-500 mb-2">
+          Snapify📸
         </h1>
 
-        <p className="text-white/80 mb-6">
+        <p className="text-black/50 mb-6">
           Capture. Save. Share.
         </p>
 
         <button
         onClick={handleClick}
-        className="w-full flex items-center justify-center gap-2 px-4 py-3 bg-white text-black rounded-xl font-semibold hover:bg-gray-100 transition">
+        className="w-full flex items-center justify-center gap-2 px-4 py-3 bg-gray-800 text-white rounded-xl font-semibold cursor-pointer transition">
           <FcGoogle size={22} />
           Continue with Google
         </button>
