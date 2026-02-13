18f1c89af51ff396ba7f748a5d2fd3eaa7d5a4ac
srushtikulkarni <srushtikulkarni09@gmail.com>
2026-02-13 10:39:31 +0530

    added all the component inside the protected route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import SuccessComponent from "./components/SuccessComponent";
 import Albums from "./pages/Albums";
 import AlbumDetails from "./pages/AlbumDetails";
 import AlbumDetail from "./pages/AlbumDetail";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 function App() {
 
@@ -13,11 +14,11 @@ function App() {
     <Toaster position="top-right" />
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login/>}/>
+        <Route path="/" element={<ProtectedRoute><Login/></ProtectedRoute>}/>
          <Route path="/success" element={<SuccessComponent />} />
-        <Route path="/albums" element={<Albums />} />
-        <Route path="/albums/name/:albumName"element={<AlbumDetails/>}/>
-        <Route path="/albums/id/:albumId" element={<AlbumDetail />} />
+        <Route path="/albums" element={<ProtectedRoute><Albums /></ProtectedRoute>} />
+        <Route path="/albums/name/:albumName"element={<ProtectedRoute><AlbumDetails/></ProtectedRoute>}/>
+        <Route path="/albums/id/:albumId" element={<ProtectedRoute><AlbumDetail /></ProtectedRoute>} />
       </Routes>
     </BrowserRouter>
     </>
