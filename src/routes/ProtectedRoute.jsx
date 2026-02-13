75399f9ec577209b76ae6b97b8f3fb63186cfae8
srushtikulkarni <srushtikulkarni09@gmail.com>
2026-02-13 12:26:39 +0530

    fixed:token error

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({children}) =>{
-    const protectedRoute = localStorage.getItem("token");
+    const token  = localStorage.getItem("token");
 
     if(!token){
         return <Navigate to="/" replace/>;
